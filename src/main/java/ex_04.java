869f4567164688cc61867bd2c3cba1e99259ffe9
Ale Bark <ale.bark_bruneri@hotmail.com>
2020-06-26 01:44:30 -0300

    ex 04

---
+++ src/main/java/ex_04.java
@@ -0,0 +1,50 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ex_04 {
+
+    public static void main(String[] args) {
+
+        // Number of financial transactions/year
+
+        Logger.getLogger("log").setLevel(Level.INFO);
+        SparkConf conf = new SparkConf().setAppName("Hello").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> transactionInputFile = sc.textFile("in/transacoes.csv");
+
+        JavaPairRDD<String, Integer> countriesCount = transactionInputFile
+                .mapToPair(getCountry())
+                .reduceByKey((x, y) -> x + y);
+
+        List<Tuple2<String, Integer>> results = countriesCount.collect();
+
+
+        Integer transactions = 0;
+        String product = null;
+
+        for (Tuple2<String, Integer> tuple : results) {
+            if (tuple._2() > transactions) {
+                product = tuple._1();
+                transactions = tuple._2();
+            }
+        }
+        System.out.println("Product with the largest number (" + transactions + ") of transactions is: " + product);
+
+    }
+
+    public static PairFunction<String, String, Integer> getCountry() {
+        PairFunction<String, String, Integer> result;
+        result = transaction -> new Tuple2<>(transaction.split(";")[3], 1);
+        return result;
+
+    }
+}
